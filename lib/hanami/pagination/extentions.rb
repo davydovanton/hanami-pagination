4432b53455db1f7d5436f0f0fa7d9dda07598231
Félix Bellanger <felix.bellanger@gmail.com>
2017-09-28 11:27:21 +0200

    Tweaked the code to avoid a deprecated warning in ROM.

--- lib/hanami/pagination/extentions.rb
+++ lib/hanami/pagination/extentions.rb
@@ -2,6 +2,6 @@ require "hanami/repository"
 
 Hanami::Repository.class_eval do
   def self.enable_pagination!
-    container.relation(relation).class.use(:pagination)
+    container.relations[relation].class.use(:pagination)
   end
 end
